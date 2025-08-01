057d82a55b8b18b66ba1b41c92828e48f6a17bdf
Francesco Casetta <francesco.casetta@gmail.com>
2025-08-01 10:59:51 +0200

    feat: client

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return "GymBro client works";
+}
+
+export default App;
---
+++ client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [solid()],
+  server: {
+    allowedHosts: ["localhost"],
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
