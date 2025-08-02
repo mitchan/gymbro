e5cc300a6c44cb9264f0878d86318732b2f0d19f
Francesco Casetta <francesco.casetta@gmail.com>
2025-08-02 09:41:15 +0200

    feat: start script and hmr

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,14 @@ import solid from "vite-plugin-solid";
 export default defineConfig({
   plugins: [solid()],
   server: {
-    allowedHosts: ["localhost"],
+    // allowedHosts: ["localhost"],
     host: "0.0.0.0",
     port: 3000,
+    hmr: {
+      clientPort: 3000,
+    },
+    watch: {
+      usePolling: true,
+    },
   },
 });
