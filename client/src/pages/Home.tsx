5a5fff0001cb2f6b3bf5673688578c832f7cb07d
Francesco Casetta <francesco.casetta@gmail.com>
2025-08-16 17:59:49 +0200

    feat: apiClient and fetch workouts

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,30 @@
-import { Show } from "solid-js";
-import { user } from "../store/app";
+import { createSignal, For, onMount, Show } from "solid-js";
+import { apiClient } from "../lib/api/apiClient";
 
 export default function Home() {
+  const [workouts, setWorkouts] = createSignal<unknown[]>([]);
+
+  onMount(() => {
+    apiClient
+      .fetch("/api/workouts")
+      .then((workouts) => {
+        if (Array.isArray(workouts)) {
+          // TODO: validate workouts
+          setWorkouts(workouts);
+        }
+      })
+      .catch((error) => {
+        // TODO: handle error
+        console.error(error);
+        setWorkouts([]);
+      });
+  });
+
   return (
-    <Show when={user()} fallback={<div>No user</div>}>
-      {(user) => {
-        return <div>{user().username}</div>;
-      }}
+    <Show when={workouts().length > 0} fallback={<div>No workouts</div>}>
+      <ul>
+        <For each={workouts()}>{() => <div>todo</div>}</For>
+      </ul>
     </Show>
   );
 }
